402cced1b7e651e332201c4b011ccd70ff839ff3
shambhu Kumar <shambhu.kumar06@GMAIL.COM>
2025-11-20 21:33:06 +0530

    Added card style

--- app/src/main/java/com/shambhu/myapplication/ExpandableActivity.kt
+++ app/src/main/java/com/shambhu/myapplication/ExpandableActivity.kt
@@ -29,7 +29,7 @@ class ExpandableActivity : AppCompatActivity() {
         binding.btnToggleAll = findViewById(R.id.btnToggleAll)*/
 
         // Customize programmatically
-        binding.expandableText5.setToggleButtonTextColor(getColor(R.color.purple_700))
+        binding.expandableText5.setToggleButtonTextColor(getColor(R.color.card_content))
         binding.expandableText5.setShowMoreText("Show more content")
         binding.expandableText5.setShowLessText("Hide content")
 
