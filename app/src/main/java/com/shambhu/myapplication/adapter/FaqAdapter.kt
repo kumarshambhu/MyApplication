1175e026f5ac4b430162522b7bf3d752bda41e3c
shambhu Kumar <shambhu.kumar06@GMAIL.COM>
2025-12-02 22:18:30 +0530

    added faq

---
+++ app/src/main/java/com/shambhu/myapplication/adapter/FaqAdapter.kt
@@ -0,0 +1,50 @@
+package com.shambhu.myapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.shambhu.myapplication.R
+import com.shambhu.myapplication.model.FaqItem
+
+class FaqAdapter(private val faqList: List<FaqItem>) :
+    RecyclerView.Adapter<FaqAdapter.FaqViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FaqViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_faq, parent, false)
+        return FaqViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FaqViewHolder, position: Int) {
+        val faqItem = faqList[position]
+        holder.bind(faqItem)
+    }
+
+    override fun getItemCount(): Int = faqList.size
+
+    inner class FaqViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val questionTextView: TextView = itemView.findViewById(R.id.questionTextView)
+        private val answerTextView: TextView = itemView.findViewById(R.id.answerTextView)
+        private val toggleIcon: ImageView = itemView.findViewById(R.id.toggleIcon)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val faqItem = faqList[position]
+                    faqItem.isExpanded = !faqItem.isExpanded
+                    notifyItemChanged(position)
+                }
+            }
+        }
+
+        fun bind(faqItem: FaqItem) {
+            questionTextView.text = faqItem.question
+            answerTextView.text = faqItem.answer
+            answerTextView.visibility = if (faqItem.isExpanded) View.VISIBLE else View.GONE
+            toggleIcon.rotation = if (faqItem.isExpanded) 45f else 0f
+        }
+    }
+}
---
+++ app/src/main/java/com/shambhu/myapplication/fragment/FaqFragment.kt
@@ -0,0 +1,59 @@
+package com.shambhu.myapplication.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.shambhu.myapplication.adapter.FaqAdapter
+import com.shambhu.myapplication.databinding.FragmentFaqBinding
+import com.shambhu.myapplication.model.FaqItem
+import com.shambhu.myapplication.utils.CommonUtils
+import org.json.JSONObject
+
+class FaqFragment : Fragment() {
+
+    private var _binding: FragmentFaqBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFaqBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val faqJson = CommonUtils.readAssetFile(requireContext(), "faq.json")
+        val faqList = parseFaqs(faqJson)
+        val adapter = FaqAdapter(faqList)
+        binding.faqRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.faqRecyclerView.adapter = adapter
+    }
+
+    private fun parseFaqs(jsonString: String): List<FaqItem> {
+        val jsonObject = JSONObject(jsonString)
+        val faqsArray = jsonObject.getJSONArray("faqs")
+        val gson = Gson()
+        val faqListType = object : TypeToken<List<FaqItem>>() {}.type
+        return gson.fromJson(faqsArray.toString(), faqListType)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun newInstance() = FaqFragment()
+    }
+}
