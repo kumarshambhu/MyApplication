e8cb0d2129514b1b68c96a314407dc94c6bbf173
shambhu Kumar <shambhu.kumar06@GMAIL.COM>
2025-12-02 22:18:43 +0530

    added

--- app/src/main/java/com/shambhu/myapplication/ExpandableActivity.kt
+++ app/src/main/java/com/shambhu/myapplication/ExpandableActivity.kt
@@ -1,21 +1,23 @@
 package com.shambhu.myapplication
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
-import com.shambhu.myapplication.adapter.ExpandableRecyclerViewAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import com.shambhu.myapplication.adapter.SecondaryNumberPagerAdapter
 import com.shambhu.myapplication.databinding.ActivityExpandableBinding
-import com.shambhu.myapplication.utils.Constants
+import com.shambhu.myapplication.utils.Constants.Companion.PREFERENCE_DATE_OF_BIRTH
+import com.shambhu.myapplication.utils.Constants.Companion.PREFERENCE_FULL_NAME
+import com.shambhu.myapplication.utils.Constants.Companion.PREFERENCE_NAME
+import com.shambhu.myapplication.utils.Constants.Companion.PREFERENCE_PLACE_OF_BIRTH
+import com.shambhu.myapplication.utils.Constants.Companion.PREFERENCE_TIME_OF_BIRTH
 
 class ExpandableActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityExpandableBinding
@@ -25,11 +27,8 @@ class ExpandableActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
         binding = ActivityExpandableBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Setup Toolbar
         setSupportActionBar(binding.toolbar)
-        supportActionBar?.title = "Personal"
 
-        // Setup Drawer
         val toggle = ActionBarDrawerToggle(
             this, binding.drawerLayout, binding.toolbar,
             R.string.navigation_drawer_open, R.string.navigation_drawer_close
@@ -39,52 +38,61 @@ class ExpandableActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
 
         binding.navView.setNavigationItemSelectedListener(this)
 
-        // Populate Nav Header
-        val sharedPref = this.getSharedPreferences(Constants.PREFERENCE_NAME, Context.MODE_PRIVATE)
-        val fullName = sharedPref?.getString(Constants.PREFERENCE_FULL_NAME, "Guest").toString()
-        val dob = sharedPref?.getString(Constants.PREFERENCE_DATE_OF_BIRTH, "0000-00-00").toString()
+        val sharedPref =
+            this.getSharedPreferences(PREFERENCE_NAME, android.content.Context.MODE_PRIVATE)
+        val fullName = sharedPref?.getString(PREFERENCE_FULL_NAME, "Guest").toString()
+        val dob = sharedPref?.getString(PREFERENCE_DATE_OF_BIRTH, "0000-00-00").toString()
+        val time = sharedPref?.getString(PREFERENCE_TIME_OF_BIRTH, "00:00")
+        val location = sharedPref?.getString(PREFERENCE_PLACE_OF_BIRTH, "Unknown Location")
+
         val headerView = binding.navView.getHeaderView(0)
         headerView.findViewById<TextView>(R.id.nav_header_full_name).text = fullName
         headerView.findViewById<TextView>(R.id.nav_header_dob).text = dob
+        headerView.findViewById<TextView>(R.id.nav_header_time).text = time
+        headerView.findViewById<TextView>(R.id.nav_header_location).text = location
 
-        // Setup Bottom Tabs
-        setupTabs()
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabs
 
-        // Original functionality
-        initViews()
-        setupClickListeners()
-        setupRecyclerViewExample()
-    }
+        viewPager.adapter = SecondaryNumberPagerAdapter(this, dob, fullName)
 
-    private fun setupTabs() {
-        binding.tabs.addTab(binding.tabs.newTab().setText("Home").setIcon(R.drawable.ic_home_filled))
-        binding.tabs.addTab(binding.tabs.newTab().setText("Secondary").setIcon(R.drawable.ic_karmic))
-        binding.tabs.addTab(binding.tabs.newTab().setText("Personal").setIcon(R.drawable.ic_pinnacle))
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.setCustomView(R.layout.custom_tab)
+            val tabIcon = tab.customView?.findViewById<ImageView>(R.id.tab_icon)
+            val tabText = tab.customView?.findViewById<TextView>(R.id.tab_text)
+            tabIcon?.setImageDrawable(
+                when (position) {
+                    0 -> getDrawable(R.drawable.ic_mic)
+                    1 -> getDrawable(R.drawable.ic_personal)
+                    2 -> getDrawable(R.drawable.ic_mirrors)
+                    else -> null
+                }
+            )
+            if (tabText != null) {
+                tabText.text = when (position) {
+                    0 -> "Elements"
+                    1 -> "Personal"
+                    2 -> "Lucky"
+                    else -> null
+                }
+            }
+        }.attach()
 
-        binding.tabs.getTabAt(2)?.select()
+        supportActionBar?.title = "Loshu Grid"
 
         binding.tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                when (tab?.position) {
-                    0 -> {
-                        val intent = Intent(this@ExpandableActivity, CoreNumberActivity::class.java)
-                        startActivity(intent)
-                    }
-                    1 -> {
-                        val intent = Intent(this@ExpandableActivity, SecondaryNumberActivity::class.java)
-                        startActivity(intent)
-                    }
-                    2 -> {
-                        // Already here
-                    }
-                }
+                val tabTextView = tab?.customView?.findViewById<TextView>(R.id.tab_text)
+                supportActionBar?.title = tabTextView?.text
             }
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
             override fun onTabReselected(tab: TabLayout.Tab?) {
-                // Do nothing
             }
         })
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -93,10 +101,12 @@ class ExpandableActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
                 val i = Intent(this, CoreNumberActivity::class.java)
                 startActivity(i)
             }
+
             R.id.nav_slideshow -> {
                 val i = Intent(this, SecondaryNumberActivity::class.java)
                 startActivity(i)
             }
+
             R.id.nav_logout -> {
                 val i = Intent(this, MainActivity::class.java)
                 startActivity(i)
@@ -106,39 +116,4 @@ class ExpandableActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
         return true
     }
 
-    private fun initViews() {
-        binding.expandableText5.setToggleButtonTextColor(getColor(R.color.card_content))
-        binding.expandableText5.setShowMoreText("Show more content")
-        binding.expandableText5.setShowLessText("Hide content")
-
-        binding.expandableText3.setOnExpandListener { isExpanded ->
-            val message = if (isExpanded) "Text expanded!" else "Text collapsed!"
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun setupClickListeners() {
-        binding.btnToggleAll.setOnClickListener {
-            binding.expandableText3.toggle()
-            binding.expandableText5.toggle()
-        }
-    }
-
-    private fun setupRecyclerViewExample() {
-        val sampleData = listOf(
-            "First item with short text",
-            "Second item with medium text that might need expansion in some cases",
-            "Third item with very long text. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            "Fourth item with another long description",
-            "Fifth short item"
-        )
-        showRecyclerView(sampleData)
-    }
-
-    private fun showRecyclerView(data: List<String>) {
-        val recyclerView = RecyclerView(this).apply {
-            layoutManager = LinearLayoutManager(this@ExpandableActivity)
-            adapter = ExpandableRecyclerViewAdapter(data)
-        }
-    }
 }
